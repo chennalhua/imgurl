adcb3a5a7898e076033bbb09dc57623211dccfe4
Naihua Chen <naihua.chen@mail.goldennet.com.tw>
2024-09-25 14:31:08 +0800

    [Update -- 20240925 -- domain setting]

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import React, { useEffect, useState } from 'react';
 const Home = () => {
     return (
         <>
-            <Image src={`https://github.com/image/user.jpg`} width={100} height={100} />
+            <Image src={`https://imgurl-pvgs.vercel.app/image/user.jpg`} width={100} height={100} />
         </>
     )
 }
